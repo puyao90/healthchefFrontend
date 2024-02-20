6f2e8eb44b7ca911ada298a9d5f3127b4e6a1c34
Yao Pu <yao.pu.-nd@disney.com>
2024-02-19 22:30:26 -0800

    revise

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,33 +3,32 @@ import smileface from "../assets/icon/smileface.svg";
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center mt-8 mb-8 md:mb-16">
-      {/* <img
-        src={logo}
-        alt="A canvas"
-        className="mb-8 w-44 h-44 object-contain"
-      /> */}
-      <h1 className="text-xl md:text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
-        Health Chef
-      </h1>
-      <div className="search">
-        <button className="w-6">
+    <header className="flex justify-between items-center ml-7 mr-8 mt-2 mb-6 md:mb-8">
+      <div className="title flex">
+        <img src={logo} alt="A canvas" className="w-12 object-contain" />
+        <h1 className="text-xl md:text-4xl font-semibold tracking-widest text-center uppercase font-title ml-2">
+          Health Chef
+        </h1>
+      </div>
+
+      <div className="rightLink flex">
+        <button className="search w-6">
           <img src={smileface} alt="" />
         </button>
-      </div>
-      <nav>
-        <div>
-          <a href="">Home</a>
+
+        <nav className="flex gap-x-6 ml-10 mr-10">
+          <div>
+            <a href="">Home</a>
+          </div>
+          <div>
+            <a href="">Recipes</a>
+          </div>
+        </nav>
+        <div className="signIn flex">
+          <img className="w-6 mr-2" src={smileface} alt="" />
+          <a href="">Sign in</a>
         </div>
-        <div>
-          <a href="">Recipes</a>
-        </div>
-      </nav>
-      <div className="signIn">
-        <img className="w-6" src={smileface} alt="" />
-        <a href="">Sign in</a>
       </div>
-      {/* <p className="text-stone-500">A community of artists and art-lovers.</p> */}
     </header>
   );
 }
